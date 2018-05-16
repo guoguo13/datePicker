1056afac73c260b81e77ba589f3fcdec7687d79c
GUOGUO13 <1147971088@qq.com>
2018-05-16 18:04:58 +0800

    更新createEl方法

--- js/datePicker.js
+++ js/datePicker.js
@@ -17,51 +17,53 @@ function DatePicker(node, options) {
         callback: function() {}
     };
     this.el = node;
+    this.elP = this.el.parentNode;
     for( var k in options ) {
         this.options[k] = options[k];
     }
     this.startDate = getDate(new Date(this.options.startDate));
+    var that = this;
 
     var createDom = function() {
-        var wrapper = cDom("div","dataPicker"),
-            container = cDom("div","container"),
-            arrow = cDom("div","arrow"),
-            prev = cDom("a","prev"),
-            next = cDom("a","next"),
-            dateBox = cDom("div","data-box");
-        arrow.appendChild(prev);
-        arrow.appendChild(next);
-        container.appendChild(arrow);
+        var prev = creEle("a", {className:"prev"} ),
+            next = creEle("a", {className:"next"} ),
+            arrow = creEle("div",{className:"arrow"}, [prev,next]),
+            calendar = createCalendar(),
+            dateBox = creEle("div", {className:"date-box"}, calendar),
+            container = creEle("div", {className:"container"}, [arrow,calendar]);
+            wrapper = creEle("div", {className:"datePicker"}, container);
+        that.elP.appendChild(wrapper);
     },
-    createTable = function () {
-        var table = cDom("table"),
-            thead = cDom("thead"),
-            theadTr = cDom("tr"); 
-        var week = ["日","一","二","三","四","五","六"];
-        var html = "";
+    createCalendar = function () {
+        var table = creEle("table"),
+            thead = creEle("thead"),
+            tbody = craeteTd(),
+            theadTr = creEle("tr"),
+            h4 = creEle("h4", {className:"month"}, that.startDate.year + '年' + that.startDate.month + '月');
+        var week = ["日","一","二","三","四","五","六"], weekHTML = '';
         for(var i = 0; i < week.length; i++) {
-            html += '<td>' + week[i] + '</td>';
+            weekHTML += '<td>' + week[i] + '</td>';
         }
-        theadTr.appendChild(html);
+        theadTr.innerHTML = weekHTML;
         thead.appendChild(theadTr);
-        table.appendChild(thead);
+        table = creEle("table", {}, [thead, tbody]);
+        return creEle("div", {className:"calendar-item"}, [h4, table]);
     },
-    creatTd = function() {
-        var tbody = cDom("tbody"); 
-        
+    createTd = function() {
+        var tbody = creEle("tbody"); 
         var curDate = getDate(new Date());
-
-
     }
+    createDom();
 };
+
 
 
 // 获取指定年的某月份有多少天
 function getDays(year,month) {
     switch(month) {
-        case 1,3,5,7,8,10,12: return 31; break;
-        case 2: return isLeapYear(year)? 28: 29; break;
-        case 4,6,9,11: return 30; break;
+        case 1,3,5,7,8,10,12: return 31;
+        case 2: return isLeapYear(year)? 28: 29;
+        case 4,6,9,11: return 30;
     }
 }
 
@@ -71,7 +73,7 @@ function getDate(day) {
     year = startDate.getFullYear(),
     month = startDate.getMonth() + 1,
     monthDays = getDays(year,month),
-    week = new Date(curYear, curMonth - 1, 1).getDay();
+    week = new Date(year, month - 1, 1).getDay();
     return { year, month, week, monthDays };
 }
 
@@ -80,13 +82,28 @@ function isLeapYear(year) {
     return year % 400 === 0 || (year % 4 ===0 && year % 100 !== 0);
 }
 
-// createDom addCLass
-function cDom (node, className = "") {
-    var el  =  document.createElement(node);
-    if(className) {
-        el.classList.add(className);
+/* 
+ * @param {type} 需创建的元素
+ * @param {props} 接收一个对象，用于添加属性, 可选
+ * @param {children} 接收一个对象，用于添加子元素, 可选
+*/
+function creEle(type, props, children) {
+    var node = document.createElement(type);
+    var childs = children != null ? [].concat(children) : [];
+    function cd(child) {
+      if(child instanceof HTMLElement){
+        return child;
+      }
+      return document.createTextNode(String(child));
     }
-    return el;
+    for(var key in props){
+      node[key] = props[key];
+    }
+
+    for(var i = 0; i < childs.length; i++) {
+        node.appendChild(cd(childs[i]));
+    }
+    return node;
 }
 
 
